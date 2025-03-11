528a902381f2d42923403a51edf8f165dc84c575
saransh619 <saranshpachhai@gmail.com>
2025-03-11 18:14:12 +0545

    cors origin url fixed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(cors({ origin: "https://ai-chatbot-talk.netlify.app/" }));
+app.use(cors({ origin: "https://ai-chatbot-talk.netlify.app" }));
 
 app.use(express.json());
 
