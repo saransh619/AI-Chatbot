bdaef036c824de18a36d0656e4271fd5f7e94f75
saransh619 <saranshpachhai@gmail.com>
2025-03-11 17:55:53 +0545

    feat: enable CORS for Netlify frontend in backend server

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(cors({ origin: "https://ai-chatbot-talk.netlify.app/" }));
+
 app.use(express.json());
 
 mongoose
